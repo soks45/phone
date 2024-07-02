9a2d97fcbb637865cc1be76ba967122d8aad68db
Aleksey Batuev <n.batuev@infinnity.ru>
2024-07-02 23:54:35 +0500

    chore(app): added lint

--- src/server/main.ts
+++ src/server/main.ts
@@ -3,21 +3,21 @@ import dotenv from 'dotenv';
 import { setupClient } from './setup-client';
 
 export function app(): express.Express {
-  const server: Express = express();
-  setupClient(server);
+    const server: Express = express();
+    setupClient(server);
 
-  return server;
+    return server;
 }
 
 function run(): void {
-  const port = process.env['SERVER_PORT'] || 4000;
+    const port = process.env['SERVER_PORT'] || 4000;
 
-  const server = app();
-  dotenv.config();
+    const server = app();
+    dotenv.config();
 
-  server.listen(port, () => {
-    console.log(`Node Express server listening on http://localhost:${port}`);
-  });
+    server.listen(port, () => {
+        console.log(`Node Express server listening on http://localhost:${port}`);
+    });
 }
 
 run();
