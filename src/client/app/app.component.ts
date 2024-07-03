358b6d8a06d73a9a9825a151e9d11c533013c52f
Aleksey Batuev <n.batuev@infinnity.ru>
2024-07-04 00:26:30 +0500

    feat(client): added tailwind

--- src/client/app/app.component.ts
+++ src/client/app/app.component.ts
@@ -1,16 +1,14 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { TuiRootModule, TuiDialogModule, TuiAlertModule } from '@taiga-ui/core';
+import { TuiRootModule, TuiDialogModule, TuiAlertModule, TuiModeModule } from '@taiga-ui/core';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, TuiRootModule, TuiDialogModule, TuiAlertModule],
+    imports: [RouterOutlet, TuiRootModule, TuiDialogModule, TuiAlertModule, TuiModeModule],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
-    title = 'phone';
-}
+export class AppComponent {}
